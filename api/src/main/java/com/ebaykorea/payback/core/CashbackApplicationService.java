0f6e8cc9253329cc014aeb9149d9fb07703a60b3
sunphong <sunphong@gmarket.com>
2022-09-27 14:13:09 +0900

    주석 추가

--- api/src/main/java/com/ebaykorea/payback/core/CashbackApplicationService.java
+++ api/src/main/java/com/ebaykorea/payback/core/CashbackApplicationService.java
@@ -16,7 +16,7 @@ public class CashbackApplicationService {
 
     //2. 리워드 정책 조회
 
-    //3. 도메인 모델 생성
+    //3. 주문 정보와 리워드 정책 정보를 통해 캐시백 도메인 모델 생성
     final var cashbacks = Cashbacks.of(orderKey); //임시 코드
 
     //4. 도메인 모델 validation?
--- api/src/main/java/com/ebaykorea/payback/port/persistence/repository/CashbackRepositoryImpl.java
+++ api/src/main/java/com/ebaykorea/payback/port/persistence/repository/CashbackRepositoryImpl.java
@@ -4,11 +4,13 @@ import com.ebaykorea.payback.core.domain.model.Cashbacks;
 import com.ebaykorea.payback.core.repository.CashbackRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CashbackRepositoryImpl implements CashbackRepository {
 
+  @Transactional
   @Override
   public void save(Cashbacks cashbacks) {
     //TODO
