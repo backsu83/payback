53cdada1d01811511031f47f8b8186f5bb1e7145
sunphong <sunphong@gmarket.com>
2022-10-07 10:08:53 +0900

    DEV DCM 적용, SP 호출 방식 변경

--- api/src/main/java/com/ebaykorea/payback/infrastructure/persistence/repository/stardb/entity/CashbackOrderEntity.java
+++ api/src/main/java/com/ebaykorea/payback/infrastructure/persistence/repository/stardb/entity/CashbackOrderEntity.java
@@ -1,25 +1,24 @@
 package com.ebaykorea.payback.infrastructure.persistence.repository.stardb.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.Instant;
 
 @Data
-@Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@IdClass(CashbackOrderEntityId.class)
-@Table(name = "CASHBACK_ORDER")
-public class CashbackOrderEntity implements Serializable {
+public class CashbackOrderEntity {
 
     //FIXME: sp 새로 만들기 (Carbon -> Payback)
-    public static final String FIND_BY_ID = "UPGMKT_Carbon_CashbackOrder_Select";
-    public static final String SAVE = "UPGMKT_Carbon_CashbackOrder_Insert";
+    public static final String FIND_BY_ID = "stardb.dbo.UPGMKT_Carbon_CashbackOrder_Select";
+    public static final String SAVE = "stardb.dbo.UPGMKT_Payback_CashbackOrder_Insert";
 
     @Id
     @Column(name = "BUY_ORDER_NO")
@@ -55,30 +54,23 @@ public class CashbackOrderEntity implements Serializable {
     private String tradeStatus;
 
     @Column(name = "USE_ENABLE_DT")
-    private Instant useEnableDt;
+    private Timestamp useEnableDt;
 
     @Column(name = "SITE_TYPE")
     private String siteType;
 
-    @Column(name = "CASHBACK_REQ_SEQ")
-    private Long cashbackReqSeq;
-
     @Column(name = "SMILE_CLUB_YN")
-    private Boolean smileClubYn;
+    private String smileClubYn;
 
     @Column(name = "SHOP_TYPE")
     private String shopType;
 
-    @Column(name = "REQ_CANCEL_YN")
-    private Boolean isRequestCancel;
-
     @Column(name = "REG_ID")
     private String regId;
 
+    @Column(name = "REG_DT")
+    private Timestamp regDt;
+
     @Column(name = "CHG_ID")
     private String chgId;
-
-
-//    @Column(name = "CLUB_DAY_MAX_SAVE_RATE")
-//    private BigDecimal clubDayMaxSaveRate;
 }
