34ae833228f3d25991c48a7bb8c7428e70023f48
sunphong <sunphong@gmarket.com>
2022-10-19 08:07:52 +0900

    temp

--- api/src/main/java/com/ebaykorea/payback/core/gateway/RewardGateway.java
+++ api/src/main/java/com/ebaykorea/payback/core/gateway/RewardGateway.java
@@ -1,8 +1,5 @@
 package com.ebaykorea.payback.core.gateway;
 
-import com.ebaykorea.payback.infrastructure.data.cashback.CashbackResponse;
-import com.ebaykorea.payback.infrastructure.gateway.client.reward.dto.CashbackRewardRequestDto;
-
 public interface RewardGateway {
-  CashbackResponse getCashbackReward(final CashbackRewardRequestDto request);
+
 }
--- api/src/main/java/com/ebaykorea/payback/infrastructure/gateway/RewardGatewayImpl.java
+++ api/src/main/java/com/ebaykorea/payback/infrastructure/gateway/RewardGatewayImpl.java
@@ -1,27 +1,14 @@
 package com.ebaykorea.payback.infrastructure.gateway;
 
-import com.ebaykorea.payback.infrastructure.data.cashback.CashbackResponse;
 import com.ebaykorea.payback.core.gateway.RewardGateway;
-import com.ebaykorea.payback.infrastructure.gateway.client.reward.dto.CashbackRewardRequestDto;
 import com.ebaykorea.payback.infrastructure.mapper.RewardGatewayMapper;
 import com.ebaykorea.payback.infrastructure.gateway.client.reward.RewardApiClient;
-import com.ebaykorea.payback.infrastructure.gateway.client.reward.dto.RewardBaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RewardGatewayImpl implements RewardGateway {
   private final RewardApiClient rewardApiClient;
   private final RewardGatewayMapper responseMapper;
-
-  public CashbackResponse getCashbackReward(final CashbackRewardRequestDto request) {
-    return Optional.ofNullable(rewardApiClient.getCashbackReward(request))
-        .filter(RewardBaseResponse::isSuccess)
-        .map(RewardBaseResponse::getResult)
-        .map(responseMapper::of)
-        .orElse(null); //TODO: 동작에 따른 올바른 리턴 결과 exception or null
-  }
 }
