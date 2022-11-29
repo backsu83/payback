365f91aa31905cfc60e10795e35bf5c8a530e3dc
백승현(seunghbaek) <seunghbaek@gmarket.com>
2022-11-29 21:10:40 +0900

    메세지 변경

--- api/src/main/java/com/ebaykorea/payback/core/domain/constant/PaybackMessageType.java
+++ api/src/main/java/com/ebaykorea/payback/core/domain/constant/PaybackMessageType.java
@@ -13,7 +13,7 @@ public enum PaybackMessageType {
   DOMAIN_ENTITY_008("{0}는 null일 수 없습니다"),
   DOMAIN_ENTITY_010("{0} 정보가 없습니다"),
   DOMAIN_ENTITY_011("결제수단이 없습니다"),
-  DOMAIN_ENTITY_013("회원이 아니면 캐시백 적립을 할 수 없습니다"),
+  DOMAIN_ENTITY_013("캐시백 적립을 할 수 없습니다"),
 
   API_GATEWAY_001("처리 실패 : {0}"),
   API_GATEWAY_002("API 조회 : {0}");
