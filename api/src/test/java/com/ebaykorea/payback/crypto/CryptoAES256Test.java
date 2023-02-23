640f4656df385d0a6d82a5e32ddd5831f8c44b8f
백승현(seunghbaek) <seunghbaek@gmarket.com>
2023-02-23 13:51:25 +0900

    java archeauth 적용 및 테스트

---
+++ api/src/test/java/com/ebaykorea/payback/crypto/CryptoAES256Test.java
@@ -0,0 +1,43 @@
+package com.ebaykorea.payback.crypto;
+
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.junit.jupiter.api.Test;
+
+public class CryptoAES256Test {
+
+  public static String alg = "AES/CBC/PKCS5Padding";
+  private final String key = "abcdefghabcdefghabcdefghabcdefgh"; // 32byte
+  private String iv = "0123456789abcdef"; // 16byte
+
+  @Test
+  void cryptText() throws Exception {
+    encryptAES256("가나다라나다");
+    decryptAES256("bXVpsKaGZ0m3EKQ0sjP/I+YFdpNk4dcOsU5Jhfx1gLk=");
+  }
+
+  // 암호화
+  void encryptAES256(String text) throws Exception {
+    Cipher cipher = Cipher.getInstance(alg);
+    SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+    IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+    byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+    System.out.println(Base64.getEncoder().encodeToString(encrypted));
+  }
+
+  // 복호화
+  void decryptAES256(String cipherText) throws Exception {
+    Cipher cipher = Cipher.getInstance(alg);
+    SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+    IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+    byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+    byte[] decrypted = cipher.doFinal(decodedBytes);
+    System.out.println(new String(decrypted, "UTF-8"));
+  }
+}
