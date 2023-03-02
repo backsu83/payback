7faeefbf5228296e82fa904abd857bde62b4902c
sunphong <sunphong@gmarket.com>
2023-03-02 15:21:21 +0900

    unit test 추가

--- api/src/main/java/com/ebaykorea/payback/infrastructure/query/data/SmilePointTradeQueryResult.java
+++ api/src/main/java/com/ebaykorea/payback/infrastructure/query/data/SmilePointTradeQueryResult.java
@@ -1,14 +1,10 @@
 package com.ebaykorea.payback.infrastructure.query.data;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SmilePointTradeQueryResult {
   private long smilePayNo;
 
@@ -25,6 +21,7 @@ public class SmilePointTradeQueryResult {
   private int apprStatus;
 
   private String apprStatusName;
+
   private int targetType;
 
   private String targetTypeName;
