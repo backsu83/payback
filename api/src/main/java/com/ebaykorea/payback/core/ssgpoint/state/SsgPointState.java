4c38101f3d0ac531b4cfbbfc847e975506672887
seunghbaek <seunghbaek@gmarket.com>
2023-03-20 16:36:15 +0900

    ssg point 예정금액 조회 추가

--- api/src/main/java/com/ebaykorea/payback/core/ssgpoint/state/SsgPointState.java
+++ api/src/main/java/com/ebaykorea/payback/core/ssgpoint/state/SsgPointState.java
@@ -1,6 +1,7 @@
 package com.ebaykorea.payback.core.ssgpoint.state;
 
 import com.ebaykorea.payback.core.domain.constant.OrderSiteType;
+import com.ebaykorea.payback.core.domain.entity.ssgpoint.SsgPointAuth;
 import com.ebaykorea.payback.core.domain.entity.ssgpoint.SsgPointStatus;
 
 public interface SsgPointState {
@@ -19,4 +20,5 @@ public interface SsgPointState {
    */
   SsgPointStatus cancel();
 
+  SsgPointAuth auth();
 }
