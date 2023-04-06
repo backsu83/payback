4ea1eba029db65af9f90c61fdf05fa9ad48c3989
백승현(seunghbaek) <seunghbaek@gmarket.com>
2023-04-06 17:48:31 +0900

    uppercase error

--- consumer/src/main/java/com/ebaykorea/payback/consumer/event/OrderCanceledAuctionEvent.java
+++ consumer/src/main/java/com/ebaykorea/payback/consumer/event/OrderCanceledAuctionEvent.java
@@ -1,14 +1,10 @@
 package com.ebaykorea.payback.consumer.event;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class OrderCanceledAuctionEvent {
 
-  @JsonProperty("OrderNo")
   private Long orderNo;
-
-  @JsonProperty("PayNo")
   private Long payNo;
 }
