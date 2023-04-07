3b7200cdef08bcf42ded656a6b702ed9525f624e
sunphong <sunphong@gmarket.com>
2023-04-07 15:48:17 +0900

    배치 테스트 disabled

--- batch/src/test/java/com/ebaykorea/payback/batch/util/support/CryptoArcheTest.java
+++ batch/src/test/java/com/ebaykorea/payback/batch/util/support/CryptoArcheTest.java
@@ -2,9 +2,11 @@ package com.ebaykorea.payback.batch.util.support;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class CryptoArcheTest {
 
