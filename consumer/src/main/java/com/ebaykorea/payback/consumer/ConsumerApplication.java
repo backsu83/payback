7622c90f7727313783d533f6aed7c3361b1465e5
sunphong <sunphong@gmarket.com>
2023-04-07 12:20:15 +0900

    import 정리

--- consumer/src/main/java/com/ebaykorea/payback/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/ebaykorea/payback/consumer/ConsumerApplication.java
@@ -3,17 +3,12 @@ package com.ebaykorea.payback.consumer;
 import com.ebaykorea.payback.consumer.config.properties.ApiInfoProperties;
 import com.ebaykorea.saturn.datasource.EnableSaturnDataSource;
 import com.ebaykorea.saturn.moa.EnableMoA;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
-
-import java.util.Arrays;
 
 @EnableMoA
 @EnableFeignClients
--- consumer/src/main/java/com/ebaykorea/payback/consumer/config/KafkaConfig.java
+++ consumer/src/main/java/com/ebaykorea/payback/consumer/config/KafkaConfig.java
@@ -2,10 +2,8 @@ package com.ebaykorea.payback.consumer.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.DefaultKafkaConsumerFactoryCustomizer;
