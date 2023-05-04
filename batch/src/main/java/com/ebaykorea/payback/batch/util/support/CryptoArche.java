235a7e71e53ad8e47faf849facd0534f8452609e
백승현(seunghbaek) <seunghbaek@gmarket.com>
2023-05-04 15:31:12 +0900

    복호화 암호화방식 코드 수정

--- batch/src/main/java/com/ebaykorea/payback/batch/util/support/CryptoArche.java
+++ batch/src/main/java/com/ebaykorea/payback/batch/util/support/CryptoArche.java
@@ -6,9 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CryptoArche {
 
-  public static String decrypt(final String message , final String siteType) throws Exception {
-    Cipher cipher = Cipher.getInstance(siteType+"-aes"); // gmarket | auction | auction-aes
-    cipher.init(Cipher.DECRYPT_MODE, siteType, "credit-card-key");
+  public static String decrypt(final String message , final String instance) throws Exception {
+    Cipher cipher = Cipher.getInstance(instance); // gmarket | auction | auction-aes
+    cipher.init(Cipher.DECRYPT_MODE, instance, "credit-card-key");
     String result = cipher.doFinal(message);
     return result;
   }
