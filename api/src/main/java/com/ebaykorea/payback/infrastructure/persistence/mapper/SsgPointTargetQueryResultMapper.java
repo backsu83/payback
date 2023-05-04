642880c2e2acc63a02fd748d874002c08d2ae5cc
minoh <minoh@gmarket.com>
2023-05-04 12:55:23 +0900

    default로 변경

--- api/src/main/java/com/ebaykorea/payback/infrastructure/persistence/mapper/SsgPointTargetQueryResultMapper.java
+++ api/src/main/java/com/ebaykorea/payback/infrastructure/persistence/mapper/SsgPointTargetQueryResultMapper.java
@@ -3,7 +3,6 @@ package com.ebaykorea.payback.infrastructure.persistence.mapper;
 import com.ebaykorea.payback.infrastructure.persistence.repository.opayreward.entity.SsgPointTargetEntity;
 import com.ebaykorea.payback.infrastructure.query.data.SsgPointTargetQueryResult;
 import com.ebaykorea.payback.util.PaybackInstants;
-import com.ebaykorea.payback.util.PaybackSavedYN;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -11,12 +10,16 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        imports = {PaybackInstants.class, PaybackSavedYN.class}
+        imports = {PaybackInstants.class}
 )
 public interface SsgPointTargetQueryResultMapper {
 
     @Mapping(source = "saveAmount", target = "ssgPointSaveAmount")
     @Mapping(expression = "java(PaybackInstants.DATE_FORMATTER.format(source.getScheduleDate()))", target = "ssgPointSaveExpectDate")
-    @Mapping(expression = "java(PaybackSavedYN.toYN(source.getPointStatus()))", target = "ssgPointSavedYN")
+    @Mapping(source = "pointStatus", target = "ssgPointSavedYN")
     SsgPointTargetQueryResult map(SsgPointTargetEntity source);
+
+    default String mapToSavedYN(final String pointStatus) {
+        return pointStatus.equals("SS") ? "Y" : "N";
+    }
 }
--- api/src/main/java/com/ebaykorea/payback/util/PaybackSavedYN.java
+++
@@ -1,7 +0,0 @@
-package com.ebaykorea.payback.util;
-
-public class PaybackSavedYN {
-    public static String toYN(final String pointStatus) {
-        return pointStatus.equals("SS") ? "Y" : "N";
-    }
-}
