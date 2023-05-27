04203d96a108a0c63fe1d375062e0af0b739959d
sunphong <sunphong@gmarket.com>
2023-05-27 12:42:30 +0900

    수

--- batch/src/main/java/com/ebaykorea/payback/batch/config/ThreadExecutorConfig.java
+++ batch/src/main/java/com/ebaykorea/payback/batch/config/ThreadExecutorConfig.java
@@ -13,7 +13,7 @@ public class ThreadExecutorConfig {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor(); // (2)
     executor.setCorePoolSize(5);
     executor.setMaxPoolSize(5);
-    executor.setQueueCapacity(10);
+    executor.setQueueCapacity(25);
     executor.setKeepAliveSeconds(0);      // QueueCapacity 초과한 max 스레드 유지 시간
     executor.setThreadNamePrefix("batch-thread-");
     executor.setWaitForTasksToCompleteOnShutdown(Boolean.TRUE);
