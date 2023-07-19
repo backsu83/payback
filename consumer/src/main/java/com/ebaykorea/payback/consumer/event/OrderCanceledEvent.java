9863df3522fb43febd45d2d712c45d796991349b
sunphong <sunphong@gmarket.com>
2023-07-19 16:51:44 +0900

    json mapper option 추가

--- consumer/src/main/java/com/ebaykorea/payback/consumer/event/OrderCanceledEvent.java
+++ consumer/src/main/java/com/ebaykorea/payback/consumer/event/OrderCanceledEvent.java
@@ -1,8 +1,15 @@
 package com.ebaykorea.payback.consumer.event;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+
 import java.util.List;
 
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = OrderCanceledAuctionEvent.class),
+    @JsonSubTypes.Type(value = OrderCanceledGmarketEvent.class)
+})
 public interface OrderCanceledEvent {
   Long getPayNo();
+
   List<Long> getOrderNos();
 }
