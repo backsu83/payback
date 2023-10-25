eb3f0d612cd6c7a0e0968252b6f5ae9ced0c6cfb
sunphong <sunphong@gmarket.com>
2023-10-25 14:22:23 +0900

    토스 사후적립 스케줄러 오류 처리

--- scheduler/src/main/java/com/ebaykorea/payback/scheduler/repository/opayreward/EventRewardRepositoryCustom.java
+++ scheduler/src/main/java/com/ebaykorea/payback/scheduler/repository/opayreward/EventRewardRepositoryCustom.java
@@ -2,12 +2,14 @@ package com.ebaykorea.payback.scheduler.repository.opayreward;
 
 import com.ebaykorea.payback.scheduler.repository.opayreward.entity.event.EventRewardRequestEntity;
 import com.ebaykorea.saturn.starter.annotation.SaturnDataSource;
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.List;
 
 import static com.ebaykorea.payback.scheduler.domain.constant.EventRequestStatusType.Requested;
@@ -21,15 +23,26 @@ import static com.querydsl.core.types.ExpressionUtils.isNull;
 public class EventRewardRepositoryCustom {
   private final JPAQueryFactory factory;
 
-  public List<EventRewardRequestEntity> findNotRequestedRequests(String tenantId) {
+  public List<EventRewardRequestEntity> findNotRequestedRequests(final String tenantId, final String startDate, final String endDate) {
     return factory.selectFrom(eventRewardRequestEntity)
         .leftJoin(eventRewardRequestEntity.statuses, eventRewardRequestStatusEntity)
         .on(eventRewardRequestStatusEntity.eventRequestStatus.eq(Requested))
-        .where(isNull(eventRewardRequestStatusEntity.requestNo), equalTenantId(tenantId))
+        .where(isNull(eventRewardRequestStatusEntity.requestNo),
+            equalTenantId(tenantId),
+            eventRewardRequestEntity.insertDate.goe(formattedDate(startDate)),
+            eventRewardRequestEntity.insertDate.lt(formattedDate(endDate)))
         .fetch();
   }
 
   private BooleanExpression equalTenantId(final String tenantId) {
     return StringUtils.hasLength(tenantId) ? eventRewardRequestEntity.tenantId.eq(tenantId) : null;
+  }
+
+  private StringExpression formattedDate() {
+    return Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m-%d')", eventRewardRequestEntity.insertDate);
+  }
+
+  private DateTemplate<Instant> formattedDate(final String dateString) {
+    return Expressions.dateTemplate(Instant.class, "to_date({0}, {1})", dateString, Expressions.constant("yyyyMMdd"));
   }
 }
