020367ad340b2c02a897b97c7571dcceba1a3213
sunphong <sunphong@gmarket.com>
2024-01-08 17:21:07 +0900

    토스 매핑 누락 수정

--- api/src/main/java/com/ebaykorea/payback/infrastructure/persistence/repository/opayreward/event/mapper/TossRewardRequestMapper.java
+++ api/src/main/java/com/ebaykorea/payback/infrastructure/persistence/repository/opayreward/event/mapper/TossRewardRequestMapper.java
@@ -1,19 +1,24 @@
 package com.ebaykorea.payback.infrastructure.persistence.repository.opayreward.event.mapper;
 
+import com.ebaykorea.payback.core.domain.constant.EventType;
 import com.ebaykorea.payback.core.domain.constant.TossRewardRequestStatusType;
 import com.ebaykorea.payback.core.dto.event.toss.TossRewardRequestDto;
 import com.ebaykorea.payback.core.domain.entity.event.TossRewardRequestResult;
 import com.ebaykorea.payback.infrastructure.persistence.repository.opayreward.event.entity.EventRewardRequestEntity;
 import com.ebaykorea.payback.infrastructure.persistence.repository.opayreward.event.entity.EventRewardRequestStatusEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
     componentModel = "spring",
-    unmappedTargetPolicy = ReportingPolicy.IGNORE
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    imports = EventType.class
 )
 public interface TossRewardRequestMapper {
 
+  @Mapping(expression = "java(EventType.Toss)", target = "eventType")
+  @Mapping(source = "source.amount", target = "saveAmount")
   EventRewardRequestEntity map(Long requestNo, String tenantId, TossRewardRequestDto source);
 
   EventRewardRequestStatusEntity map(Long requestNo, TossRewardRequestStatusType eventRequestStatus);
