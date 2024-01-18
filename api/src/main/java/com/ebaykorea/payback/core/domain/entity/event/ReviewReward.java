6b425a76f4b7a3956394e747b7a18dacdc20449e
sunphong <sunphong@gmarket.com>
2024-01-18 13:21:31 +0900

    옥션 상품평 적립은 대량적립 요청 상태로 입력

--- api/src/main/java/com/ebaykorea/payback/core/domain/entity/event/ReviewReward.java
+++ api/src/main/java/com/ebaykorea/payback/core/domain/entity/event/ReviewReward.java
@@ -20,6 +20,7 @@ public class ReviewReward extends SmileCashEvent {
   private static final int EXPIRATION_PERIOD = 180;
   private static final String NORMAL_TOUR_REWARD_COMMENT = "여행 후기 기본 적립";
   private static final String PREMIUM_TOUR_REWARD_COMMENT = "여행 후기 추가 적립";
+  private static final int MASS_REQUEST_READY = 3;
 
   public ReviewReward(
       final long requestNo,
@@ -48,6 +49,11 @@ public class ReviewReward extends SmileCashEvent {
       default:
         return "";
     }
+  }
+
+  @Override
+  public int getRequestStatus() {
+    return MASS_REQUEST_READY;
   }
 
   private boolean isNormalReviewType() {
