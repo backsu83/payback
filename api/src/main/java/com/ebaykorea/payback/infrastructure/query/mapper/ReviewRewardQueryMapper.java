ef54f359437457515c906afdbefb94f4360f0712
백승현(seunghbaek) <seunghbaek@gmarket.com>
2024-01-19 10:02:46 +0900

    saveDate 조건 부여

--- api/src/main/java/com/ebaykorea/payback/infrastructure/query/mapper/ReviewRewardQueryMapper.java
+++ api/src/main/java/com/ebaykorea/payback/infrastructure/query/mapper/ReviewRewardQueryMapper.java
@@ -5,6 +5,8 @@ import com.ebaykorea.payback.infrastructure.persistence.repository.auction.maind
 import com.ebaykorea.payback.infrastructure.persistence.repository.gmkt.stardb.entity.SmileCashEventEntity;
 import com.ebaykorea.payback.infrastructure.query.data.ReviewRewardQueryResult;
 import com.ebaykorea.payback.util.PaybackInstants;
+import java.sql.Timestamp;
+import java.time.Instant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -23,7 +25,7 @@ public interface ReviewRewardQueryMapper {
 
   @Mapping(expression = "java(mapToReasonCode(entity.getReasonCode()))", target = "reviewType")
   @Mapping(expression = "java(mapToSaveStatus(entity.getSaveStatus()))" , target = "save")
-  @Mapping(expression = "java(PaybackInstants.from(entity.getUpdateDate()))" , target = "saveDate")
+  @Mapping(expression = "java(mapToSaveDate(entity.getUpdateDate() , entity.getSaveStatus()))" , target = "saveDate")
   ReviewRewardQueryResult map(SmileCashSaveQueueEntity entity);
 
 
@@ -32,4 +34,12 @@ public interface ReviewRewardQueryMapper {
   default boolean mapToSaveStatus(int status) {return status == 1;}
 
   default EventType mapToReasonCode(String code) {return EventType.auctionCodeOf(code);}
+
+  default Instant mapToSaveDate(Timestamp date, int status) {
+    if(status == 1) {
+      return PaybackInstants.from(date);
+    }
+    return null;
+  }
+
 }
